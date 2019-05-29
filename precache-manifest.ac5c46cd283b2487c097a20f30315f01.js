48eba09c61762e8e83ffea534fa4b61d45c4a138
Vladimir Gornyj <vladimir.gornyj@piano.io>
2019-05-29 10:41:13 +0300

    Updates

---
+++ precache-manifest.ac5c46cd283b2487c097a20f30315f01.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "07a26feda38f2391bd88",
+    "url": "/post-editor/static/css/main.c87c6aca.chunk.css"
+  },
+  {
+    "revision": "07a26feda38f2391bd88",
+    "url": "/post-editor/static/js/main.07a26fed.chunk.js"
+  },
+  {
+    "revision": "1874a3d4cfe55b6e22a0",
+    "url": "/post-editor/static/js/1.1874a3d4.chunk.js"
+  },
+  {
+    "revision": "f9e008eb25fbbb24f702",
+    "url": "/post-editor/static/js/runtime~main.f9e008eb.js"
+  },
+  {
+    "revision": "e76a46cf07dc33b40a052aada3c37f9a",
+    "url": "/post-editor/static/media/instagram_post.e76a46cf.png"
+  },
+  {
+    "revision": "8ddbd15f9a4e75744dbe4dce4d330687",
+    "url": "/post-editor/static/media/background_photo.8ddbd15f.jpg"
+  },
+  {
+    "revision": "ec7b2bd7dd0d2ea37092a91be96445a3",
+    "url": "/post-editor/static/media/Ossem-Rounded.ec7b2bd7.woff2"
+  },
+  {
+    "revision": "f469d91b8dd9c073791d322ef59f36af",
+    "url": "/post-editor/static/media/Ossem-Rounded.f469d91b.woff"
+  },
+  {
+    "revision": "40354da633e824d2fb219aa3346db061",
+    "url": "/post-editor/index.html"
+  }
+];
